babeec8e4d073619f48539fa527487c0ce63c913
mishra709s <mishra.709s@gmail.com>
2025-03-23 13:56:21 +0530

    operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -43,8 +43,8 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = Symbol()
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
 
 // 33 => "33"
 // true => "true"
@@ -57,3 +57,43 @@ console.log(typeof stringNumber)
 // Symbol() => "Symbol()"
 
 /* Convert To String End */
+
+/********************* Operations *********************/
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = 'hello'
+let str2 = ' soubhagya'
+
+let str3 = str1 + str2
+// console.log(str3); // hello soubhagya
+
+// console.log("1" + 2); // "12"
+// console.log(1 + "2"); // "12"
+// console.log("1" + 2 + 2); // "122"
+// console.log(1 + 2 + "2"); // "32"
+
+// console.log( (3 + 4) * 5 % 3); // 2
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
