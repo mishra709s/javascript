ffa86f0cd07f21ef1bf5e372099a3fb208d201e9
mishra709s <mishra.709s@gmail.com>
2025-03-27 01:29:57 +0530

    objects part 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,9 @@
+// const tinderUser = new Object() // Object constructor or signleton object
+const tinderUser = {}
+
+tinderUser.id = '123abc'
+tinderUser.name = 'John Doe'
+tinderUser.age = 25
+tinderUser.isLoggerIn = false
+
+console.log(tinderUser)
