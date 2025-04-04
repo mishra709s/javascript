5f2911d58fe8c58771f3c563419d6065a5bea8c6
mishra709s <mishra.709s@gmail.com>
2025-04-05 00:18:32 +0530

    scopes in js part-2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,3 +9,30 @@ if (true) {
 // console.log(a);
 // console.log(b);
 // console.log(c);
+
+// Nested Scropes
+
+function one() {
+  const username = 'soubhagya'
+
+  function two() {
+    const website = 'youtube'
+    console.log(username)
+  }
+  console.log(website)
+  two()
+}
+one()
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+console.log(addOne(5))
+
+function addOne(num) {
+  return num + 1
+}
+
+addTwo(5)
+const addTwo = function (num) {
+  return num + 2
+}
